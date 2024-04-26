9b892c85f8d1a05ffad5c3f48cdafa5d71def92d
Victex4 <eberevictory3@gmail.com>
2024-04-26 12:21:47 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const menuOpen = document.querySelector('.ri-menu-line')
+const menuClose = document.querySelector('.ri-close-line')
+const menu = document.querySelector('nav ul')
+
+menuOpen.addEventListener('click', function(){
+    menu.style.top = '30%'
+    menuClose.style.display = 'block'
+    menuOpen.style.display = 'none'
+})
+
+menuClose.addEventListener('click', function(){
+    menu.style.top = '-30%'
+    menuClose.style.display = 'none'
+    menuOpen.style.display = 'block'
+})
+
+document.querySelectorAll('nav ul li').forEach(item => {
+    item.addEventListener('click', function(){
+        menu.style.top = '-30%'
+        menuClose.style.display = 'none'
+        menuOpen.style.display = 'block'
+    })
+})
